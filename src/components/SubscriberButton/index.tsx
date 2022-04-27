3248e960849f8b7173a3b244fd63ff883f3f3cde
Vinícios Barbosa <viniciosbarbosa740@gmail.com>
2022-04-27 09:22:29 -0300

    feat: feita a página Home

---
+++ src/components/SubscriberButton/index.tsx
@@ -0,0 +1,5 @@
+import styles from "./styles.module.scss";
+
+export function SubscriberButton() {
+  return <button className={styles["subscriber-button"]}>Subscribe now</button>;
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,63 @@
-import Head from "next/head";
+import { GetServerSideProps } from "next";
+import { stripe } from "../services/stripe";
 
-export default function Home() {
+import Head from "next/head";
+import Image from "next/image";
+
+import { SubscriberButton } from "../components/SubscriberButton";
+
+import styles from "../styles/home.module.scss";
+
+interface HomeProps {
+  product: {
+    priceId: string;
+    price: number;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
-        <title>ig.news</title>
+        <title>Home - ig.news</title>
       </Head>
+      <main className={styles["content-conteiner"]}>
+        <section className={styles.content}>
+          <span>👏 Hi, welcome!</span>
+          <h1>
+            News about the <span>React</span> world
+          </h1>
+          <p>
+            Get acess to all the publications
+            <br />
+            <strong>for ${product.price}/month</strong>
+          </p>
+          <SubscriberButton />
+        </section>
+        <Image
+          height={521}
+          width={336}
+          src="/images/avatar.svg"
+          alt="girl coding"
+        />
+      </main>
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve("price_1Kt86DB9Y1hCSAVJKrWi2fzi", {
+    expand: ["product"],
+  });
+
+  const product = {
+    priceId: price.id,
+    price: price.unit_amount / 100,
+  };
+
+  return {
+    props: {
+      product,
+    },
+  };
+};
