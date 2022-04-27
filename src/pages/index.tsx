90b44a9cdeb50ba90888abbabe60cc7de9be88b8
Vinícios Barbosa <viniciosbarbosa740@gmail.com>
2022-04-27 09:27:56 -0300

    refactor: getServerSideProps trocado por getStaticProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import { stripe } from "../services/stripe";
 
 import Head from "next/head";
@@ -45,7 +45,7 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve("price_1Kt86DB9Y1hCSAVJKrWi2fzi", {
     expand: ["product"],
   });
@@ -59,5 +59,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       product,
     },
+    revalidate: 24 * 60 * 60, // 24 hours
   };
 };
